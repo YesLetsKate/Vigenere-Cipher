958f95ee2f02ecec91f7c1107f2b2ab1833dfe23
YesLetsKate <85181945+YesLetsKate@users.noreply.github.com>
2023-04-04 16:02:25 +0700

    Добавьте файлы проекта.

---
+++ Vigenere Сipher/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Vigenere_Сipher;
+
+Console.Write("Текст:  ");
+string text =  Console.ReadLine();
+Console.Write("Ключ:  ");
+string key = Console.ReadLine();
+VigenereCipher vigenereCipher = new VigenereCipher();
+string cipher = vigenereCipher.Encrypt(text, key);
+Console.Write("Шифр:  ");
+Console.WriteLine(cipher);
+
+string opentxt = vigenereCipher.Decrypt(cipher, key);
+Console.Write("Открытый текст:  ");
+Console.WriteLine(opentxt);
---
+++ Vigenere Сipher/VigenereCipher.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vigenere_Сipher
+{
+    internal class VigenereCipher
+    {
+        static string Ralf = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        int n = Ralf.Length;
+        public VigenereCipher() { }
+        public string Encrypt(string text,string key)
+        {
+            string cipher = "";
+            int i = 0;
+            int j = 0;
+            while (true)
+            {
+                int index = Ralf.IndexOf(text[i]);
+                int keyindex = Ralf.IndexOf(key[j]);
+                int newindex = Modulo(index + keyindex,n);
+                cipher = cipher + Ralf[newindex];
+                i++;
+                j = Modulo(j + 1,key.Length);
+                try { char k = text[i]; }
+                catch { break; }
+            }
+            return cipher;
+        }
+        public string Decrypt(string text, string key)
+        {
+            string opentext = "";
+            int i = 0;
+            int j = 0;
+            while (true)
+            {
+                int index = Ralf.IndexOf(text[i]);
+                int keyindex = Ralf.IndexOf(key[j]);
+                int newindex = Modulo(index - keyindex, n);
+                opentext = opentext + Ralf[newindex];
+                i++;
+                j = Modulo(j + 1, key.Length);
+                try { char k = text[i]; }
+                catch { break; }
+            }
+            return opentext;
+        }
+        private int Modulo(int a, int n)
+        {
+            if (a >= n || a < 0)
+            {
+                if (a < 0)
+                {
+                    a = a % n;
+                    a = a + n;
+                }
+                else a = a % n;
+            }
+            return a;
+        }
+    }
+}
